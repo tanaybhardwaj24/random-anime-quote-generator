e7562c05beb01752d2df8444fa24c96161378552
tanaybhardwaj24 <tanaybhardwaj24@gmail.com>
2021-06-28 13:58:50 +0000

    new version

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <GetJokes />
+</div>
+</template>
+
+<script>
+import GetJokes from './components/GetJokes.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GetJokes
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GetJokes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <p>{{data.setup}}</p>
+    <p>{{data.punchline}}</p>
+     <button class="btn btn-lg btn-primary" @click.prevent="getJoke">Get Joke</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "GetJokes",
+  data() {
+    return {
+      data: {}
+    }
+  },
+  methods: {
+    async getJoke(){
+      const res = await fetch('https://official-joke-api.appspot.com/random_joke');
+      const data = await res.json();
+      this.data = data;
+      console.log("joke sent")
+    }
+  },
+};
+</script>
+<style scoped>
+#app {
+    font-family: sans-serif;
+    font-size: 30px;
+    max-width: 400px;
+    margin: auto;
+    height: 85vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
