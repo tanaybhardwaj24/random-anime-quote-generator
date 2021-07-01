a3688b956b341e1cf286baf0e35f54a6ca20a88c
tanaybhardwaj24 <tanaybhardwaj24@gmail.com>
2021-07-01 06:39:07 +0000

    styling

--- src/components/GetJokes.vue
+++ src/components/GetJokes.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <p>{{data.setup}}</p>
-    <p>{{data.punchline}}</p>
-     <button class="btn btn-lg btn-primary" @click.prevent="getJoke">Get Joke</button>
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <p class="card-text">{{data.setup}}</p>
+    <p class="card-text">{{data.punchline}}</p>
+    <button class="btn btn-lg btn-primary btn btn-dark" @click.prevent="getJoke">Get Joke</button>
+  </div>
+</div>
   </div>
 </template>
 <script>
@@ -26,7 +30,7 @@ export default {
 </script>
 <style scoped>
 #app {
-    font-family: sans-serif;
+    font-family: monospace;
     font-size: 30px;
     max-width: 400px;
     margin: auto;
